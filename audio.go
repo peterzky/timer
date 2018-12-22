bd00a14ffc6fc891b0041f56a73979da065cb036
peterzky <peter.zky@qq.com>
2018-12-22 17:56:10 +0800

    find audio file in nixos

--- audio.go
+++ audio.go
@@ -1,17 +1,13 @@
 package main
 
 import (
+	"os"
 	"os/exec"
 	"path"
-	"runtime"
 )
 
 func play(f string) {
-	_, filename, _, ok := runtime.Caller(0)
-	if !ok {
-		panic("No caller information")
-	}
-
+	filename, _ := os.Executable()
 	cmd := exec.Command("aplay", path.Dir(filename)+"/../sound/"+f+".wav")
 	cmd.Run()
 }
